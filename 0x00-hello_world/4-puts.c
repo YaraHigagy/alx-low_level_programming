1cfcd371ea47caa3d661d08256a540d5875efc8c
Yara Higagy <yara.higagy@gmail.com>
2023-08-23 13:22:11 -0700

    Modifying-1 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
+/*
+* File: 4-puts.c
+*/
+
 #include <stdio.h>
 
+/*
+* main - prints "Programming is like building a multilingual puzzle,
+* followed by a new line.
+
+Return: Always 0 (Success)
+*/
 int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
