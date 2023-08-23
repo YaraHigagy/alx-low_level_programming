610a255d8c6e7ed2ccecd29e1c79724f6b2d13d3
Yara Higagy <yara.higagy@gmail.com>
2023-08-23 14:38:24 -0700

    Modifying-1 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
 
 /*
 * main - prints with proper grammar, but the outcome is a piece of art,,
-* followed by a new line.
-
-Return: Always 0 (Success)
+* 		followed by a new line.
+*
+* Return: Always 0. (Success)
 */
 int  main(void)
 {
