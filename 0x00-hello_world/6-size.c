257716ce997c20eb915d8a42e6e560486b2af11b
Yara Higagy <yara.higagy@gmail.com>
2023-08-23 14:43:36 -0700

    Modifying 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,21 @@
 
 #include <stdio.h>
 
-/*
-* main - Prints the size of various types on the computer it is compiled and run on.
+/**
+ * main - Prints the size of various types on the computer it is compiled and run on.
+ *
+ *Return: Always 0 (Success)
+ */
 
-Return: Always 0 (Success)
-*/
 int  main(void)
 {
-	char *arr[] = {"char", "int", "long int", "long long int", "float"}
+	char *arr[] = {"char", "int", "long int", "long long int", "float"};
+
 	for (int i = 0; i < 5; i++)
 	{	
 		printf("Size of a %s: %zu byts(s)\n", arr[i], sizeof(arr[i]));	
 	}
+
 	return (0);
 }
 
