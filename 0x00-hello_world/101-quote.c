382116d4b688cf1d42a5655b03a7f3ddc2a6a49c
Yara Higagy <yara.higagy@gmail.com>
2023-08-24 12:47:43 -0700

    Modifying-4 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,8 +13,8 @@
 
 int main(void)
 {
-	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(2, buf, strnlen(buf,sizeof(buf)));
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
