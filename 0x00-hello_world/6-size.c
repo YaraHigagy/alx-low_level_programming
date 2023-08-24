41c5b50fa60606ffee19ead079f1eae7f973a6da
Yara Higagy <yara.higagy@gmail.com>
2023-08-24 10:54:29 -0700

    Modifying-7 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,21 @@
 
 int  main(void)
 {
-
+/*
 	char *arr[] = {"char", "int", "long int", "long long int", "float"};
 	int i;
 	
 	for (i = 0; i < 5; i++)
 	{
-    		printf("Size of a %s: %zu byte(s)\n", arr[i], sizeof(arr[i]));
+    		printf("Size of a $s: %zu byte(s)\n", arr[i], sizeof(arr[i]));
 	}
-
+*/
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	
 	return (0);
 }
 
