6437cdfc461a6e86cb36eec12423b0dd09e5a2df
Yara Higagy <yara.higagy@gmail.com>
2023-08-25 07:13:04 -0700

    Creating 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always (0).
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('\n');
+
+	return (0);
+}
