dcc0c14ac83b64228d2220624f70f105f1463a62
Yara Higagy <yara.higagy@gmail.com>
2023-08-25 09:35:43 -0700

    Creating 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digit,
+ *	and every two digits are different and unique,
+ *	separated by ', ',
+ *	followed by a new line.
+ *
+ * Return: Always (0).
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			continue;
+
+		for (j = i+1; j < 10; j++)
+		{
+			if (j != i)
+				putchar('0' + i);
+				putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
