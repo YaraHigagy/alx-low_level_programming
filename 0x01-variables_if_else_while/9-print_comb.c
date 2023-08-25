d774be82da8f887c8125f849183e0bbcd97772d8
Yara Higagy <yara.higagy@gmail.com>
2023-08-25 08:39:21 -0700

    Creating 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers,
+ *	separated by ', ',
+ *	followed by a new line.
+ *
+ * Return: Always (0).
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar('0' + x);
+		if (x == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
