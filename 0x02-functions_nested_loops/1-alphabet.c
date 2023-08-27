e4b470250bc45c274b91579f63908e5c16bbd110
Yara Higagy <yara.higagy@gmail.com>
2023-08-27 10:58:36 -0700

    Creating 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Description: prints alphabet in lowercase,
+ * 	followed by a newline.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
