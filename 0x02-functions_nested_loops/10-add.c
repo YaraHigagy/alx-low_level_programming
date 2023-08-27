dbb1604269cf470b6c0a7f7d6106ea2467674058
Yara Higagy <yara.higagy@gmail.com>
2023-08-27 14:50:32 -0700

    Creating 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @n: integer to be tested.
+ * @m: integer to be tested.
+ *
+ * Return: n + m.
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
