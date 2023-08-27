81a86b8f5fff9ce381c1405267680d03a56976b5
Yara Higagy <yara.higagy@gmail.com>
2023-08-27 16:43:14 -0700

    Creating 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main  - computes the absolute value of an integer.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i, sum3, sum5, sum_total;
+
+	for (i = 3; i < 1023; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum3 += sum3;
+		}
+		if (i >= 5 && i % 3 == 0)
+		{
+			sum5 += sum5;
+		}
+	}
+	sum_total = sum3 + sum5;
+	printf("%d", sum_total);
+
+	return (sum_total);
+}
