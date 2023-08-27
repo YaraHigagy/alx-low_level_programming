b284a8c230dfe2732b48bb32d89b8025a340f613
Yara Higagy <yara.higagy@gmail.com>
2023-08-27 11:36:59 -0700

    Modifying 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,17 @@
 
 /**
  * _islower -  checks for lowercase character.
+ * @c: integer to be tested.
+ *
+ * ReturnL 1 or 0.
  */
 
 int _islower(int c)
 {
 	if (islower(c) != 0)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 	/* The code below is good to */
 	/* return (c >= 97 && c <= 122) */
 }
