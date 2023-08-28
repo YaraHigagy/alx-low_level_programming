84682536004d0786b637cfc73af14e36881ec758
Yara Higagy <yara.higagy@gmail.com>
2023-08-27 17:34:01 -0700

    Modifying-7 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -31,26 +31,26 @@ int sumMul(int start, int end, int step)
 
 int main(void)
 {
-		/*
-	int i, sum3 = 0, sum5 = 0, sum15 = 0, sum_total;
-
-	for (i = 0; i < 1023; i++)
-	{
-		if (i >= 3 && i % 3 == 0)
-		{
-			sum3 += i;
-		}
-		if (i >= 5 && i % 5 == 0)
-		{
-			sum5 += i;
-		}
-		if (i >=15 && i % 15 == 0)
-		{
-			sum15 += i;
-		}
-	}
-	sum_total = sum3 + sum5 - sum15;
-	printf("%d\n", sum_total);
+	/**
+	*int i, sum3 = 0, sum5 = 0, sum15 = 0, sum_total;
+	*
+	*for (i = 0; i < 1023; i++)
+	*{
+	*	if (i >= 3 && i % 3 == 0)
+	*	{
+	*		sum3 += i;
+	*	}
+	*	if (i >= 5 && i % 5 == 0)
+	*	{
+	*		sum5 += i;
+	*	}
+	*	if (i >=15 && i % 15 == 0)
+	*	{
+	*		sum15 += i;
+	*	}
+	*}
+	*sum_total = sum3 + sum5 - sum15;
+	*printf("%d\n", sum_total);
 	*/
 
 	int sum3, sum5, sum15, sum_total;
