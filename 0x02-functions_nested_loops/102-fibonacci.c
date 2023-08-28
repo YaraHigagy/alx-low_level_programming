289402c5fac118655cbdfdf241ee2e1c6cb29d42
Yara Higagy <yara.higagy@gmail.com>
2023-08-28 10:50:21 -0700

    Creating 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ *	separated by comma, foloowed by a space,
+ *	and followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i, f_1 = 1, f_2 = 2, f;
+
+	printf("1, 2");
+
+	for (i = 4; i <= 50; i++)
+	{
+		f = f_1 + f_2;
+
+		printf(", %d", f);
+
+		f_1 = f_2;
+		f_2 = f;
+	}
+
+	printf("\n");
+
+	return (0);
+}
