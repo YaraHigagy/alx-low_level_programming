5a02c11294fd00593da7297fdb08f4217d97e86a
Yara Higagy <yara.higagy@gmail.com>
2023-08-29 02:23:29 -0700

    Creating 0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Prints whether the number stored in the variable n is:
+ *		positive, negative, or zero.
+ * Return: Always (0).
+ */
+int positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
